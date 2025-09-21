a73b4d0d2ddcf73f702b2e1af92ceb161183aecf
Xuty <xutyxd@gmail.com>
2025-09-21 12:30:02 +0200

    fix(exports): added classes and interface to exports to working with it

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// Client
 import { ESIOSApiClient } from './classes/esios-api-client.class';
+// Classes
+import { PVPCDay } from './classes/pvpc-day.class';
+import { PVPCHour } from './classes/pvpc-hour.class';
+// Interfaces
+import { IPVPCHourNormalized } from './interfaces/pvpc-hour-normalized.interface';
 
-export { ESIOSApiClient };
+export { ESIOSApiClient, PVPCDay, PVPCHour, IPVPCHourNormalized };
