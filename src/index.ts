f11427430e89a8d709efc5609f51985ccf1a0043
Xuty <xutyxd@gmail.com>
2025-09-21 13:08:40 +0200

    fix(exports): exported PVPCDayZoned

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 import { ESIOSApiClient } from './classes/esios-api-client.class';
 // Classes
 import { PVPCDay } from './classes/pvpc-day.class';
+import { PVPCDayZoned } from './classes/pvpc-day-zoned.class';
 import { PVPCHour } from './classes/pvpc-hour.class';
 // Interfaces
 import { IPVPCHourNormalized } from './interfaces/pvpc-hour-normalized.interface';
 
-export { ESIOSApiClient, PVPCDay, PVPCHour, IPVPCHourNormalized };
+export { ESIOSApiClient, PVPCDay, PVPCDayZoned, PVPCHour, IPVPCHourNormalized };
