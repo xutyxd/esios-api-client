7b24dfa0125aa7dbb373af3f2a011f606ee21229
Xuty <xutyxd@gmail.com>
2025-09-21 12:35:10 +0200

    fix(raw): save raw to work with base source

--- src/classes/pvpc-hour.class.ts
+++ src/classes/pvpc-hour.class.ts
@@ -1,11 +1,13 @@
 import { IPVPCHourNormalized } from "../interfaces/pvpc-hour-normalized.interface";
 
 export class PVPCHour {
+    public readonly raw: IPVPCHourNormalized;
     public readonly day: Date;
     public readonly hour: number;
     public readonly price: number;
 
     constructor(hour: IPVPCHourNormalized) {
+        this.raw = hour;
         this.day = new Date(hour.day);
         this.hour = Number(hour.hour.split('-')[0]);
         this.price = Number(hour.total.replace(',', '.'));
