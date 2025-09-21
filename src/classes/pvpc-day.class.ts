c3260f0b1114775eccd188d093bd48502acb2e5f
Xuty <xutyxd@gmail.com>
2025-09-21 10:37:53 +0200

    feat(date): added date to PVPCDay

--- src/classes/pvpc-day.class.ts
+++ src/classes/pvpc-day.class.ts
@@ -3,10 +3,12 @@ import { PVPCDayZonedGeneral } from "./pvpc-day-zoned-general.class";
 import { PVPCDayZonedSpecial } from "./pvpc-day-zoned-special.class";
 
 export class PVPCDay {
+    public date: Date;
     public general: PVPCDayZonedGeneral;
     public special: PVPCDayZonedSpecial;
 
     constructor(day: IPVPCDay) {
+        this.date = new Date(day.PVPC[0].Dia);
         this.general = new PVPCDayZonedGeneral(day.PVPC);
         this.special = new PVPCDayZonedSpecial(day.PVPC);
     }
