2ed9e0ee46f48f2131ac516606eee3232c0e3184
Xuty <xutyxd@gmail.com>
2025-09-25 00:22:19 +0200

    fix(export): added Indicator class to export

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import { ESIOSApiClient } from './classes/esios-api-client.class';
 import { PVPCDay } from './classes/pvpc/pvpc-day.class';
 import { PVPCDayZoned } from './classes/pvpc/pvpc-day-zoned.class';
 import { PVPCHour } from './classes/pvpc/pvpc-hour.class';
+import { Indicator } from './classes/indicator/indicator.class';
 // Interfaces
 import { IPVPCHourNormalized } from './interfaces/pvpc/pvpc-hour-normalized.interface';
 // Enums
 import { ArchiveID, IndicatorID, Geo } from './enums';
 
-export { ESIOSApiClient, PVPCDay, PVPCDayZoned, PVPCHour, IPVPCHourNormalized, ArchiveID, IndicatorID, Geo };
+export { ESIOSApiClient, PVPCDay, PVPCDayZoned, PVPCHour, IPVPCHourNormalized, Indicator, ArchiveID, IndicatorID, Geo };
