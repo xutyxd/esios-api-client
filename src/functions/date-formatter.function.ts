8270e175c263a2d1697b376b169b10e0e08eb62d
Xuty <xutyxd@gmail.com>
2025-10-02 01:09:07 +0200

    fix(times): handle sign correctly

--- src/functions/date-formatter.function.ts
+++ src/functions/date-formatter.function.ts
@@ -16,8 +16,9 @@ export function formatDate(date: Date): string {
     const tzOffsetMinutes = new Date().toLocaleTimeString('es-ES', { timeZone: 'Europe/Madrid', timeZoneName: 'shortOffset' })
         .match(/GMT([+-]\d{1,2})(\d{2})?/);
 
-    const sign = tzOffsetMinutes ? tzOffsetMinutes[1][0] : '+';
-    const tzHours = tzOffsetMinutes ? tzOffsetMinutes[1].padStart(2, '0') : '00';
+    const [sign, ...hours] = (tzOffsetMinutes ? tzOffsetMinutes[1] : '+0').split('');
+    const hoursJoined = hours.join('');
+    const tzHours = hoursJoined ? hoursJoined.padStart(2, '0') : '00';
     const tzMinutes = tzOffsetMinutes && tzOffsetMinutes[2] ? tzOffsetMinutes[2].padStart(2, '0') : '00';
 
     return `${lookup.year}-${lookup.month}-${lookup.day} ${lookup.hour}:${lookup.minute}:${lookup.second} ${sign}${tzHours}${tzMinutes}`;
